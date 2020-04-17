fd1a5b1dec9992b19d8c505bdaea04083f7d62b9
Michael Villander <nousvillander@gmail.com>
2020-04-17 01:39:58 -0300

    Add lazy-engine to tests

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.mount('lazy-engine', { path: '/use-lazy-engine', as: 'use-lazy-engine' });
+});
 
 export default Router;
